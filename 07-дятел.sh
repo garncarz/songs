e82f907b4ea45262103a7f79380186b8d7d6664a
Ondřej Garncarz <ondrej@garncarz.cz>
2017-01-29 01:22:35 +0100

    дятел build script finished.
    Ending sample added.

--- 07-дятел.sh
+++ 07-дятел.sh
@@ -2,11 +2,10 @@
 
 cd out
 
-wget https://upload.wikimedia.org/wikipedia/commons/5/5b/Woodpecker.ogg \
-    -O woodpecker.ogg
+# wget https://upload.wikimedia.org/wikipedia/commons/5/5b/Woodpecker.ogg \
+#      -O woodpecker.ogg
 
 ln -s 07-дятел.flac 07-dyatel.flac
 
-# FIXME volume & padding
-sox -v 0.8 woodpecker.ogg -c 2 -p pad 130 | \
+sox -v 0.3 ../woodpecker.ogg -c 2 -p pad 129.22077 | \
     sox - -m 07-dyatel.flac 07-dyatel-final.flac
