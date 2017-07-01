b98591c2ef85e6784129bf940c7d51f4d81a6da1
Ondřej Garncarz <ondrej@garncarz.cz>
2017-07-01 04:48:49 +0200

    Full PO in Mido.

--- po_midi.py
+++ po_midi.py
@@ -17,7 +17,9 @@ class Scale:
 
 
 f_major = Scale(65, 'major', 'F')
+f_minor = Scale(65, 'minor', 'Fm')
 c_minor = Scale(60, 'minor', 'Cm')
+g_minor = Scale(67, 'minor', 'Gm')
 
 assert c_minor[0] == 60
 assert c_minor[1] == 62
@@ -32,38 +34,54 @@ class Track(MidiTrack):
     def __init__(self, midi_file=None):
         self.parent = midi_file
         self.shift = 0
-        self._rest = 0
-        self.beats_stolen = 0
+        self.shift_in_scale = 0
+        self._beats_to_rest = 0
+        self._beats_stolen = 0
+        self._scale = None
 
     def play(self, tones, beats=1, grace=False):
+        if tones in [None, 'r', 'R']:
+            return self.rest(beats)
+
+        if beats == 'grace':
+            return self.grace(tones)
+
         if not isinstance(tones, list):
             tones = [tones]
 
-        note = lambda tone: self.scale[tone] + 12 * self.shift
+        note = lambda tone: self.scale[tone + self.shift_in_scale] + 12 * self.shift
         time = lambda beats: int(beats * self.parent.ticks_per_beat)
 
-        note_on = lambda tone, time: self.append(Message('note_on', note=note(tone), velocity=100, time=time))
-        note_off = lambda tone, time: self.append(Message('note_off', note=note(tone), time=time))
+        note_on = lambda tone, beats: self.append(
+            Message('note_on', note=note(tone), velocity=100, time=time(beats))
+        )
+        note_off = lambda tone, beats: self.append(
+            Message('note_off', note=note(tone), time=time(beats))
+        )
 
-        note_on(tones[0], time(self._rest))
-        self._rest = 0
+        note_on(tones[0], self._beats_to_rest)
+        self._beats_to_rest = 0
         for tone in tones[1:]:
             note_on(tone, 0)
 
         if grace:
-            note_off(tones[0], time(beats))
-            self.beats_stolen += beats
+            note_off(tones[0], beats)
+            self._beats_stolen += beats
         else:
-            note_off(tones[0], time(beats - self.beats_stolen))
-            self.beats_stolen = 0
+            note_off(tones[0], beats - self._beats_stolen)
+            self._beats_stolen = 0
         for tone in tones[1:]:
             note_off(tone, 0)
 
-    def grace(self, tones, portion=16):
+    def sequence(self, sequence):
+        for play_args in sequence:
+            self.play(*play_args)
+
+    def grace(self, tones, portion=8):
         self.play(tones, beats=1.0/portion, grace=True)
 
-    def rest(self, rest):
-        self._rest = rest
+    def rest(self, beats):
+        self._beats_to_rest += beats
 
     @property
     def scale(self):
@@ -71,8 +89,9 @@ class Track(MidiTrack):
 
     @scale.setter
     def scale(self, scale):
-        self._scale = scale
-        self.append(MetaMessage('key_signature', key=scale.signature))
+        if scale != self._scale:
+            self._scale = scale
+            self.append(MetaMessage('key_signature', key=scale.signature))
 
     @property
     def bpm(self):
@@ -102,18 +121,7 @@ class Track(MidiTrack):
         self.append(Message('program_change', program=instrument))
 
 
-mid = MidiFile()
-
-bass = Track(mid)
-chords = Track(mid)
-violin = Track(mid)
-
-
-bass.scale = f_major
-bass.shift = -2
-bass.bpm = 180
-
-for _ in range(8):
+def bassline():
     for _ in range(4):
         bass.play(0)
         bass.play(4)
@@ -122,65 +130,242 @@ for _ in range(8):
         bass.play(5)
 
 
-chords.scale = f_major
+def bassline_bridge():
+    bass.scale = c_minor
+    bass.sequence([(4, 2), (7, 2),
+                   (4,), (7,), (4,), (7,)])
 
-chords.rest(4 * 4)
+    bass.scale = g_minor
+    bass.sequence([(4, 2), (0, 2),
+                   (0,), (4,), (0,), (4,)])
 
-for _ in range(2):  # TODO variations are not exactly correct
-    chords.play([0, 2, 4], 4)
-    chords.grace(-3)
-    chords.play([2, 4, 7], 4)
-    chords.grace(-2)
-    chords.play([2, 5, 7], 4)
+    bass.scale = f_major
+    bass.sequence([(4, 2), (7, 2)])
+
+
+def bassline_outro():
+    for _ in range(2):
+        bassline()
+
+    bass.shift_in_scale = -1
+    bassline()
+
+    bass.shift_in_scale = -2
+    bassline()
+
+    bass.shift_in_scale = 0  # cleaning
+
+
+def chords_verse(variation):
+    assert -1 <= variation <= 3
+
+    if variation == -1:
+        chords.play([0, 2, 4], 4)
+        chords.grace(-3)
+        chords.play([2, 4, 7], 4)
+        chords.grace(-2)
+    elif variation in [0, 1, 2]:
+        chords.play([0, 2, 4], 1)
+        chords.play([-3, -1, 1], 3)
+        chords.play([0, 2, 4], 4)
+    elif variation == 3:
+        chords.play([0, 2, 4], 1)
+        chords.play([-3, -1, 1], 2)
+        chords.play([-3, -1, 1], 1)
+        chords.grace(7)
+        chords.play([0, 2, 4], 4)
+
+    if variation in [-1, 0]:
+        chords.play([2, 5, 7], 4)
+    elif variation == 1:
+        chords.play([2, 5, 7], 1)
+        chords.play([0, 3, 5], 3)
+    elif variation == 2:
+        chords.play([2, 5, 7], 1)
+        chords.play([3, 5, 7], 3)
+    elif variation == 3:
+        chords.play([2, 5, 7], 1)
+        chords.play([0, 3, 5], 2)
+        chords.play([0, 3, 5], 1)
+        chords.grace(-2)
+
     chords.play([0, 2, 5], 4)
 
-    chords.play([0, 2, 4], 1)
-    chords.play([-3, -1, 1], 3)
-    chords.play([0, 2, 4], 4)
-    chords.play([2, 5, 7], 4)
-    chords.play([0, 2, 5], 4)
+
+def chords_chorus(variation):
+    assert 0 <= variation <= 2
 
     chords.play([0, 2, 4], 1)
-    chords.play([-3, -1, 1], 3)
+    chords.grace(3)
+    chords.play([-1, 1, 4], 3)
+
+    if variation == 2:
+        chords.sequence([
+            (0, 'grace'),
+            (2, 'grace'),
+            (4, 'grace'),
+        ])
     chords.play([0, 2, 4], 4)
+
     chords.play([2, 5, 7], 1)
-    chords.play([0, 3, 5], 3)
+    if variation in [0, 2]:
+        chords.play([0, 3, 5], 3)
+    elif variation == 1:
+        chords.play([3, 5, 7], 3)
+
+    if variation == 2:
+        chords.sequence([
+            (5, 'grace'),
+            (2, 'grace'),
+            (0, 'grace'),
+        ])
     chords.play([0, 2, 5], 4)
 
-    chords.play([0, 2, 4], 1)
-    chords.play([-3, -1, 1], 3)
-    chords.play([0, 2, 4], 4)
-    chords.play([2, 5, 7], 1)
-    chords.play([3, 5, 7], 3)
-    chords.play([0, 2, 5], 4)
+
+def chords_bridge():
+    chords.scale = c_minor
+    chords.sequence([
+        ([4, 6, 8], 2),
+        ([4, 7, 9], 2),
+        ([4, 6, 8], 1),
+        ([4, 7, 9], 3),
+    ])
+
+    chords.scale = g_minor
+    chords.sequence([
+        ([-1, 1, 4], 2),
+        (3, 'grace'),
+        ([0, 2, 4], 2),
+        ([-1, 1, 4], 1),
+        (3, 'grace'),
+        ([0, 2, 4], 3),
+    ])
+
+    chords.scale = f_major
+    chords.sequence([
+        ([-1, 1, 4], 2),
+        ([0, 2, 4],),
+        ([1, 4, 6],),
+    ])
 
 
-violin.scale = f_major
+def chords_outro():
+    for v in [2, 1]:
+        chords_chorus(v)
+
+    chords.shift_in_scale = -1
+    chords_chorus(1)
+
+    chords.shift_in_scale = -2
+    chords_chorus(1)
+
+    chords.shift_in_scale = 0  # cleaning
+
+
+def violin_verse():
+    for _ in range(2):
+        violin.sequence([
+            (0,), (1,), (4,), (1,), (0,), (2,), (4,), (2,),
+            (-2, 2), (3, 2),
+            (0,), (-2,), (2, 2),
+        ])
+
+    violin.sequence([
+        (-3, 2), (-1, 2), (0, 2),
+        (4,), (4,), (5, 2),
+        (3,), (3,), (0,), (1,), (4,), (1,),
+    ])
+
+
+def violin_chorus():
+    violin.sequence([
+        (4, 4), (None, 4),
+        (2,), (5,), (7, 2), (None, 4),
+        (4, 4), (0, 4), (2, 2),
+        (3,), (0,), (5, 4),
+    ])
+
+
+def intro():
+    bass.scale = f_major
+    chords.scale = f_major
+    violin.scale = f_major
+
+    for _ in range(4):
+        bassline()
+
+    chords.rest(4 * 4)
+    for v in [-1, 0, 1]:
+       chords_verse(v)
+
+    violin.rest(4 * 16)
+
+
+def verse():
+    bass.scale = f_major
+    chords.scale = f_major
+    violin.scale = f_major
+
+    for _ in range(8):
+        bassline()
+
+    for v in [2, 1, 1, 2, 3, 1, 1, 2]:
+        chords_verse(v)
+
+    for _ in range(2):
+        violin.rest(4 * 4)
+        violin_verse()
+
+
+def chorus():
+    bass.play(4, 2)
+    bass.scale = f_minor
+    bass.play(-2, 2)
+    for _ in range(6):
+        bassline()
+
+    chords.play([1, 4, 6], 2)
+    chords.scale = f_minor
+    chords.play([0, 2, 7], 2)
+    for v in [0, 1, 0, 0, 1, 0]:
+        chords_chorus(v)
+
+    violin.scale = f_minor
+    violin.rest(4 * 5)
+    for _ in range(2):
+        violin_chorus()
+    violin.rest(4 * 4)
+
+
+def bridge():
+    bassline_bridge()
+    chords_bridge()
+    violin.rest(4 * 5)
+
+
+def outro():
+    bassline_outro()
+    chords_outro()
+    violin.rest(4 * 16)
+
+
+mid = MidiFile()
+
+bass = Track(mid)
+chords = Track(mid)
+violin = Track(mid)
+
+bass.shift = -2
+bass.bpm = 180
+
 violin.instrument = 41
 
-violin.rest(4 * 20)
-
-for _ in range(2):
-    for tone in [0, 1, 4, 1, 0, 2, 4, 2]:
-        violin.play(tone)
-
-    violin.play(-2, 2)
-    violin.play(3, 2)
-
-    violin.play(0)
-    violin.play(-2)
-    violin.play(2, 2)
-
-violin.play(-3, 2)
-violin.play(-1, 2)
-violin.play(0, 2)
-
-violin.play(4)
-violin.play(4)
-violin.play(5, 2)
-
-for tone in [3, 3, 0, 1, 4, 1]:
-    violin.play(tone)
-
+intro()
+verse()
+chorus()
+bridge()
+verse()
+chorus()
+outro()
 
 mid.save('po_midi.mid')
