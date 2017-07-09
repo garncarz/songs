034734efe3e91555b968e13092dbb6bb4c2bb4b9
Ondřej Garncarz <ondrej@garncarz.cz>
2017-07-10 01:14:53 +0200

    Pop G-dur pythonized (percussion free).
    Working with MIDI channels.
    #13: scale can be defined just by a root & intervals family.
    #14: instrument names table.

---
+++ src.py/03-pop_g-dur.py
@@ -0,0 +1,182 @@
+from itertools import cycle
+
+from midi_lib import *
+
+
+g_major = Scale('g', 'major')
+
+
+def line(*tones, beats):
+    if isinstance(beats, list):
+        return list(zip(tones, cycle(beats)))
+    return list(map(lambda t: (t, beats), tones))
+
+
+def chopped_line(*chords, times, beats=2):
+    for chord in chords:
+        for _ in range(times):
+            yield (chord, beats/times)
+
+
+def piano_line():
+    ending = [([-1, 1, 4], .5), ([-1, 1, 4], 1.5), ([-3, -1, 1], 2)]
+    verse_1 = line(
+        0, 2, 0, -3, [0, 2], 3, [2, 4], 3, [1, 4], 0,
+        [-1, 1], 0, [-3, -1], 0, [-1, 1], 0,
+        [0, 2], -1, [-2, 0], -1, [0, 2], 4, [2, 5], 4,
+        beats=[3/4, 1/4],
+    ) + ending
+    verse_2 = line(
+        [0, 2], -2, [-3, 0], -2, [0, 2], 3, [2, 4], 3,
+        [1, 4], 0, [-1, 1], 0, [-3, -1], 0, [-1, 1], 0,
+        [0, 2], -1, [-2, 0], -1, [0, 2], 4, [2, 5], 4,
+        beats=[3/4, 1/4],
+    ) + ending
+
+    piano.sequence([
+        *line([-3, 0, 2], [0, 2, 4], [-1, 1, 4], [-3, -1, 1], [-2, 0, 2],
+              [0, 2, 5], [-1, 1, 4], [-3, -1, 1], 'r', beats=2),
+
+        *verse_1,
+        *verse_2,
+        *verse_2,
+        *verse_1,
+        *verse_2,
+
+        ('r', 33*2),
+
+        *verse_1,
+        *verse_2,
+        ([-3, 0, 2], 2),
+    ])
+
+
+def sax_line():
+    sax.shift_in_scale = +2
+    assert sax._note(0) == 59  # but actually it sounds like 56... wow
+
+    verse_1 = [
+        *line(0, 0, -1, -1, -3, -3, beats=[.5, 1.5]),
+        *line(-2, 0, -1, beats=2), -1, 2, (5, 2),
+    ]
+    verse_2 = [
+        *line(1, 0, -2, -1, -2, -3, beats=[3 / 4, 1 + 1 / 4]),
+        *line(-2, 'r', 0, -2, 'r', -4, beats=[.5, .5, 1]),
+        *line(-5, 2, beats=2),
+    ]
+    verse_2_1 = verse_2 + [(5, 2)]
+    verse_2_2 = verse_2 + [(5, 2*2)]
+    verse_2_3 = verse_2 + [(-2, 2)]
+
+    sax.sequence([
+        ('r', 10*2),
+
+        *verse_1,
+        *verse_2_1,
+        ('r', 8*2),
+        *verse_1,
+        *verse_2_2,
+
+        ('r', 8*2),
+
+        *verse_1,
+        *verse_2_1,
+        ('r', 8*2),
+        *verse_1,
+        *verse_2_3,
+    ])
+
+
+def bass_line():
+    bass.default_beats = 2
+
+    verse = [0, -3, -3, -2, -2, -3, -3, 0]
+
+    bass.sequence([
+        ('r', 10*2),
+
+        *verse,
+
+        *verse,
+        *verse[:-1], ('r', 2),
+        *verse,
+        *verse[:-1],
+
+        ('r', 10*2),
+
+        *verse,
+        *verse,
+        *verse[:-1], ('r', 2),
+        *verse,
+        *verse,
+    ])
+
+
+def guitar_line():
+    chords = [[-3, 0, 2,], [0, 2, 4], [-1, 1, 4],
+              [-3, -1, 1], [-2, 0, 2], [0, 2, 5]]
+
+    verse = [
+        *chopped_line(*chords, times=4),
+        ([-1, 1, 4], .5), ([-1, 1, 4], 1.5),
+        ([-3, -1, 1], 2),
+    ]
+
+    def intermezzo(*chords, beats=.5):
+        for chord in chords:
+            yield from [(chord, beats), (chord[0], beats), (chord, beats), (chord[0], beats)]
+
+    guitar.sequence([
+        ('r', 50*2),
+
+        *chopped_line(*chords, [-1, 1, 4], [-3, -1, 1], times=2),
+
+        *verse,
+        *verse,
+
+        *intermezzo([-3, 0, 2], [0, 2, 4], [-1, 1, 4], [-3, -1, 1],
+                    [2, 0, -2], [5, 2, 0], [4, 1, -1], [1, -1, -3]),
+
+        *verse,
+        *verse,
+        [-3, 0, 2], [-3, 0, 2],
+    ])
+
+
+def song():
+    piano_line()
+    sax_line()
+    bass_line()
+    guitar_line()
+
+
+def make():
+    global mid, piano, sax, guitar, bass
+
+    mid = MidiFile()
+
+    for channel, track_name in enumerate(['piano', 'sax', 'guitar', 'bass']):
+        track = globals()[track_name] = Track(mid, channel)
+        track.scale = g_major
+        track.time_signature = 2, 4
+
+    piano.instrument = instruments['bright acoustic piano']
+    sax.instrument = instruments['baritone sax']
+    guitar.instrument = instruments['electric guitar (clean)']
+    bass.instrument = instruments['acoustic bass']
+
+    piano.bpm = 160
+    sax.shift = -1
+    bass.shift = -2
+
+    song()
+
+
+if __name__ == '__main__':
+    make()
+    mid.save('../out/03-pop_g-dur.midi')
+
+    from pprint import pprint
+    from midi_diff import diff
+    d = diff('../midi/03-pop_g-dur.midi', '../out/03-pop_g-dur.midi', normalize_times=False)
+    pprint(d)
